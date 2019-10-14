19b9095e0d185d0be55318270dc0249909a5c4d7
Ubuntu <ubuntu@ip-172-31-93-184.ec2.internal>
2019-10-14 13:56:33 +0000

    javascript files

--- script.js
+++ script.js
@@ -1,59 +1,135 @@
-const cards = document.querySelectorAll('.memory-card');
+// the Match Game function which contains all functionality of the game
+class MatchGame {
+    constructor (cards, totalTime){
+        this.cardsArray = cards;
+        this.totalTime = totalTime;
+        this.timeRemaining = totalTime;
+        this.timer = document.getElementById('time-remaining');
+        this.ticker = document.getElementById('flips');
+    }
+    // start game function initiate the game
+    startGame(){
+        this.cardToCheck = null;
+        this.totalClicks = 0;
+        this.timeRemaining = this.totalTime;
+        this.matchedCards = [];
+        this.busy = true;
+        setTimeout(function ()  {
+            this.shuffle();
+            this.countDown = this.startCountDown();
+            this.busy = false;
+        }, 500);
+        this.hideCards();
+        this.timer.innerText = this.timeRemaining;
+        this.ticker.innerText = this.totalClicks;
+       
+    }
+    
+    // hide the cards when they are not clicked
+    
+   hideCards() {
+        this.cardsArray.forEach(card => {
+            card.classList.remove('flip');
+        });
+    }
 
-let hasFlippedCard = false;
-let lockBoard = false;
-let firstCard, secondCard;
+ // flip card function 
+ 
+  flipCard(card) {
+      if(this.canFlipCard(card)) {
+            this.totalClicks++;
+            this.ticker.innerText = this.totalClicks;
+            card.classList.add('flip');
 
-function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
-
-  this.classList.add('flip');
-  if (!hasFlippedCard) {
-    hasFlippedCard = true;
-    firstCard = this;
-
-    return;
+            if(this.cardToCheck)
+                this.checkForMatch(card);
+            else
+                this.cardToCheck = card;
+        
+        }
   }
+  
+   // checking for match
+   
+  checkForMatch(card) {
+        if(this.getAttribute('data-card') === this.getAttribute(this.cardToCheck)){
+            this.cardMatch(card, this.cardToCheck);
+       } else {
+            this.disableCards(card, this.cardToCheck);
+}
+        this.cardToCheck = null;
+    }
+    
+    // card match pushes matched cards to an array
+    
+    cardMatch(card1, card2) {
+        this.matchedCards.push(card1);
+        this.matchedCards.push(card2);
+        if(this.matchedCards.length === this.cardsArray.length)
+        this.victory(); 
+    }
+    
+    // disable cards when they are not a match
+    
+    disableCards(card1, card2) {
+        this.busy = true;
+        setTimeout(() => {
+            card1.classList.remove('flip');
+            card2.classList.remove('flip');
+            this.busy = false;
+        }, 1000);
+    }
+    startCountDown() {
+        return setInterval(() => {
+            this.timeRemaining--;
+            this.timer.innerText = this.timeRemaining;
+            if(this.timeRemaining === 0)
+                this.gameOver();
+        }, 1000);
+    }
+    gameOver() {
+        clearInterval(this.countDown);
+        document.getElementById('game-over-text').classList.add('visible');
+    }
+    victory() {
+        clearInterval(this.countDown);
+        document.getElementById('victory-text').classList.add('visible');
+    }
 
-  secondCard = this;
-  checkForMatch();
+    shuffle () {
+        for(let i = this.cardsArray.length - 1; i > 0; i--) {
+            let randIndex = Math.floor(Math.random() * (i+1));
+            this.cardsArray[randIndex].style.order = i;
+            this.cardsArray[i].style.order = randIndex;
+        }
+    }
+
+    canFlipCard(card) {
+        return !this.busy && !this.matchedCards.includes(card) && card !== this.cardToCheck;
+    }
+    
 }
 
-function checkForMatch() {
-  let isMatch = firstCard.dataset.card === secondCard.dataset.card;
+function ready() {
+    let textGame = Array.from(document.getElementsByClassName('text-game'));
+    let cards = Array.from(document.getElementsByClassName('memory-card'));
+    let game = new MatchGame(100, cards);
 
-  isMatch ? disableCards() : unflipCards();
+    textGame.forEach( function(textGame) {
+        textGame.addEventListener('click', function () {
+            textGame.classList.remove('visible');
+            game.startGame();
+        });
+    });
+    cards.forEach(function (card) {
+    card.addEventListener('click', function () {
+        game.flipCard(card);
+    });
+});
 }
 
-function disableCards() {
-  firstCard.removeEventListener('click', flipCard);
-  secondCard.removeEventListener('click', flipCard);
-
-  resetBoard();
+if(document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', ready());
+} else {
+    ready();
 }
-
-function unflipCards() {
-  lockBoard = true;
-
-  setTimeout(() => {
-    firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');
-
-    resetBoard();
-  }, 1500);
-}
-
-function resetBoard() {
-  [hasFlippedCard, lockBoard] = [false, false];
-  [firstCard, secondCard] = [null, null];
-}
-
-(function shuffle() {
-  cards.forEach(card => {
-    let randomPos = Math.floor(Math.random() * 12);
-    card.style.order = randomPos;
-  });
-})();
-
-cards.forEach(card => card.addEventListener('click', flipCard))
---
+++ script2.js
@@ -0,0 +1,99 @@
+
+
+class MatchGame {
+    constructor () {
+    this.cards = Array.from(document.getElementsByClassName('memory-card'));
+    this.cardsToCheck = [];
+    this.cardsFlipped = 0; // click event remove
+    this.cardsMatched = [];
+    
+    
+    
+    this.cards.forEach(card => {
+        card.addEventListener('click', () => {
+            this.handleClickCard(card);
+        });
+    });
+    
+    }
+    
+    handleClickCard(card) {
+        // check if card is matched
+        
+        // route 1 -> card is already matched 
+        // route 2 -> card is not matched
+        console.log(card)
+        
+        this.flipCard(card)
+        
+        this.cardsToCheck.push(card)
+        
+        console.log(this.cardsToCheck);
+        
+        if (this.cardsToCheck.length === 2) {
+            
+            const matchFound = this.checkForMatch()
+            
+            if (matchFound) {
+                console.log('match')
+                this.cardsMatched.push(...this.cardsToCheck);   
+            } else {
+                console.log('no match')
+                this.flipCard(this.cardsToCheck[0]);
+                this.flipCard(this.cardsToCheck[1]);
+            }
+            this.cardsToCheck = []
+        }
+    }
+    
+    checkForMatch() {
+        // if match: store the matched cards
+        var dataAttr1 = this.cardsToCheck[0].getAttribute('data-card');
+        var dataAttr2= this.cardsToCheck[1].getAttribute("data-card");
+        
+        if (dataAttr1 === dataAttr2){
+            return true
+        }
+        
+        return false
+    }
+    
+    flipCard(card){
+    // if this card is clicked add flip
+    // check if card has 'flip' class
+   if (card.classList.contains('flip')){
+       card.classList.remove("flip");
+   }else{
+       card.classList.add("flip");
+   };
+   
+    
+}
+
+}
+
+
+
+
+
+
+function ready () {
+    let textGame = Array.from(document.getElementsByClassName('text-game'));
+    let game = new MatchGame();
+
+    textGame.forEach(textGame => {
+        textGame.addEventListener('click', () => {
+            textGame.classList.remove('visible');
+            game.startGame();
+        });
+    });
+    
+   
+}
+
+if(document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', ready());
+} else {
+    ready();
+}
+
